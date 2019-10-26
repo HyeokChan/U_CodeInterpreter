fde44fa57f2a48bdfdba9e5045073a8400ff3cb2
HyeokChan <48059565+HyeokChan@users.noreply.github.com>
2019-10-26 19:44:54 +0900

    label, asm, val1, val2, val3

---
+++ CodeTable_.java
@@ -0,0 +1,20 @@
+package level3;
+
+//CodeTable ¿˙¿Â
+public class CodeTable_ {
+	String label;
+	String asm;
+	String val1;
+	String val2;
+	String val3;
+	
+	public CodeTable_(String label, String asm, String val1, String val2, String val3) 
+	{
+		this.label = label;
+		this.asm = asm; 
+		this.val1 = val1; 
+		this.val2 = val2; 
+		this.val3 = val3; 
+	}
+}
+
